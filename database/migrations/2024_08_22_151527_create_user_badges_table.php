a9e93bea57d444f5503d67673bf6f845244198c6
yogiefani <yogiefaniyc@gmail.com>
2025-06-27 21:09:29 +0700

    fix: create user badges migration

--- database/migrations/2024_08_22_151527_create_user_badges_table.php
+++ database/migrations/2024_08_22_151527_create_user_badges_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_badges', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('badge_id');
+            $table->timestamp("earned_at")->useCurrent();
             
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('badge_id')->references('id')->on('badges')->onDelete('cascade');
