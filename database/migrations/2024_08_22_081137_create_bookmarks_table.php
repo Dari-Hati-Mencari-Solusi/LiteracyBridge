310ec223f45162ffe4a61c8a59ba6abe26c95452
yogiefani <yogiefaniyc@gmail.com>
2025-06-27 20:53:14 +0700

    fix: migration bookmarks table

--- database/migrations/2024_08_22_081137_create_bookmarks_table.php
+++ database/migrations/2024_08_22_081137_create_bookmarks_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("user_id");
             $table->unsignedBigInteger("book_id");
-
+            $table->integer("page_number")->nullable();
+            $table->string("note")->nullable();
             $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
             $table->foreign("book_id")->references("id")->on("books")->onDelete("cascade");
             $table->timestamps();
